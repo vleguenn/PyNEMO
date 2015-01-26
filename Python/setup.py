5d2b3981b912dda32e3ec0ed3ee2fd4b2047f27a
sri.nagella <sri.nagella@atlassian.com>
2015-01-26 13:08:37 +0000

    [#1211] added basemap for the required modules
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/pynemo/trunk@57 9a1c3b68-90a4-42a4-98f4-a1cd0af2bb7b

--- Python/setup.py
+++ Python/setup.py
@@ -40,7 +40,7 @@ setup(
       
       packages=['pynemo','pynemo.tests','pynemo.gui'],
       
-      install_requires=['netCDF4','scipy','numpy','matplotlib'],
+      install_requires=['netCDF4','scipy','numpy','matplotlib', 'basemap'],
       
       include_package_data=True,
       #The data files that needs to be included in packaging
