7be6b50c93eea4816473a27a2a364d3b317e12ba
sri.nagella <sri.nagella@atlassian.com>
2015-02-09 14:02:09 +0000

    [#1126] tidied setup with comments
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/pynemo/trunk@60 9a1c3b68-90a4-42a4-98f4-a1cd0af2bb7b

--- Python/pynemo/nemo_bdy_setup.py
+++ Python/pynemo/nemo_bdy_setup.py
@@ -3,7 +3,6 @@
 #######                                          #######
 ## Written by John Farey, August 2012                 ##
 ########################################################
-from __builtin__ import str
 
 '''
 Invoke with a text file location, class init reads and deciphers variables.
@@ -11,168 +10,69 @@ Invoke with a text file location, class init reads and deciphers variables.
 attribute 'settings' is a dict holding all the vars.
 '''
 
-import os.path
 import logging
-import sys
-class Setup:
 
-    # setfile = settings file location
+class Setup(object):
+    """ This class holds the settings information """
     def __init__(self, setfile):
+        """ Constructor, reads the settings file and sets the dictionary with setting name/key and
+        it's value """
         #Logging for class
-        logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(__name__)
         self.filename = setfile
         if not setfile: # debug
             self.filename = '../data/namelist.bdy'
         try:
             namelist = open(self.filename, 'r')
         except:
-            logger.error("Cannot open the file:"+setfile)
+            self.logger.error("Cannot open the file:"+setfile)
             raise
         data = namelist.readlines()
-        
-        # Dictionary of all the vars in the file and a seperate settings for boolean values        
-        self.settings, self.bool_settings = self._assign(self._trim(data))
-        
+        # Dictionary of all the vars in the file and a seperate settings for boolean values
+        self.settings, self.bool_settings = _assign(_trim(data))
         namelist.close()
 
-# # # # # # #
-# Functions #
-# # # # # # #
-
-    # Trims comments and whitespace
-    def _trim(self, data):
-        for l in range(len(data)):
-            x = data.pop(0)
-            x = x.rsplit('!')[0].strip()
-            if x is not '':
-                data.append(x)
-
-        return data
-
-    # x = string, checks type, returns name and val. Raises error for ambiguous
-    def _get_val(self, vars, bool_vars, x):
-        t = x[0][0:2].lower()
-        n = x[0][3:].lower().strip() # 3 -> 0 to keep type info
-        v = x[1].strip()
-
-       
-        if t == 'ln':
-            if v.find('true') is not -1:
-                if vars.has_key(n) != True:
-                    vars[n] = True
-                bool_vars[n] = True
-            elif v.find('false') is not -1:
-                if vars.has_key(n) != True:
-                    vars[n] = False
-                bool_vars[n] = False
-            else:
-                raise ValueError('Cannot assign %s to %s, must be boolean' 
-                                                                    %(v,n))
-        elif t == 'rn' or t == 'nn':
-            if v.find('.') > -1 or v.find('e') > -1:
-                try:
-                    vars[n] = float(v)
-                except ValueError:
-                    print 'Cannot assign %s to %s, must be float'
-                    raise
-            else:
-                try:
-                    vars[n] = int(v)
-                except ValueError:
-                    print 'Cannot assign %s to %s, must be integer'
-                    raise
-        elif t == 'cn' or t == 'sn':              
-                vars[n] = v.strip("'")                
-        elif t == 'cl':
-                name = x[0].split('(')
-                index = name[1].split(')')
-                if name[0].strip() not in vars.keys():
-                    vars[name[0].strip()] = {}
-                vars[name[0].strip()][index[0].strip()]=v.strip()
-        else:
-            raise ValueError('%s data type is ambiguous' %x)
-
-
-    def _get_var_name_value(self,line):
-        x = line.split("=",2)
-        t = x[0][0:2].lower()
-        name = x[0][3:].lower().strip() # 3 -> 0 to keep type info    
-        v = x[1].strip()
+    def _get_var_name_value(self, line):
+        """ splits the line into key value pair. """
+        key_value = line.split("=", 2)
+        name_prefix = key_value[0][0:2].lower()
+        name = key_value[0][3:].lower().strip() # 3 -> 0 to keep type info
+        value_str = key_value[1].strip()
         index = '-1'
         value = ''
-        if t == 'ln':
-            if v.find('true') is not -1:
+        if name_prefix == 'ln':
+            if value_str.find('true') is not -1:
                 value = True
-            elif v.find('false') is not -1:
+            elif value_str.find('false') is not -1:
                 value = False
             else:
-                raise ValueError('Cannot assign %s to %s, must be boolean' 
-                                                                    %(v,name))
-        elif t == 'rn' or t == 'nn':
-            if v.find('.') > -1 or v.find('e') > -1:
+                raise ValueError('Cannot assign %s to %s, must be boolean' %(value_str, name))
+
+        elif name_prefix == 'rn' or name_prefix == 'nn':
+            if value_str.find('.') > -1 or value_str.find('e') > -1:
                 try:
-                    value = float(v)
+                    value = float(value_str)
                 except ValueError:
-                    print 'Cannot assign %s to %s, must be float'
+                    self.logger.error('Cannot assign %s to %s, must be float')
                     raise
             else:
                 try:
-                    value = int(v)
+                    value = int(value_str)
                 except ValueError:
-                    print 'Cannot assign %s to %s, must be integer'
+                    self.logger.error('Cannot assign %s to %s, must be integer')
                     raise
-        elif t == 'cn' or t == 'sn':              
-                value = v.strip("'")                
-        elif t == 'cl':
-                name = x[0].split('(')
-                index = name[1].split(')')
-                name = name[0].strip()
-                index = index[0].strip()
-                value = v                
+        elif name_prefix == 'cn' or name_prefix == 'sn':
+            value = value_str.strip("'")
+        elif name_prefix == 'cl':
+            name = key_value[0].split('(')
+            index = name[1].split(')')
+            name = name[0].strip()
+            index = index[0].strip()
+            value = value_str
         else:
-            raise ValueError('%s data type is ambiguous' %x)
+            raise ValueError('%s data type is ambiguous' %key_value)
         return name, index, value
 
-                
-    def _replace_var_value(self,original_line,name,index,value, new_value):
-        if type(value).__name__ == 'bool':
-            value = str(value).lower()
-            new_value = str(new_value).lower()
-        elif type(value).__name__ == 'str': #an empty string need to search for ''
-            print value, new_value
-            if value == '':
-                value = '\'\''
-                new_value = '\''+new_value+'\''             
-        return original_line.replace(value,new_value,1)
-    
-    def _get_var_name(self,line):
-        x = line.split("=",2)
-        t = x[0][0:2].lower()
-        name = x[0][3:].lower().strip() # 3 -> 0 to keep type info        
-        if t in ['ln', 'rn','nn','cn','sn']:
-            return name, -1
-        elif t == 'cl':
-            name = x[0].split('(')
-            index = name[1].split(')')
-            return name, index
-    
-    # Returns tidy dictionary of var names and values
-    def _assign(self, data):
-        vars = {}
-        bool_vars = {}
-        for line in data:
-            x = line.split('=', 2)
-            self._get_val(vars, bool_vars, x)
-#            n,v = self._get_val(x)
-#            vars[n] = v
-
-        return vars, bool_vars
-
-    def strip_comments(self,line):
-        line = line.rsplit('!')[0].strip()
-        return line
-    
-        
     def write(self):
         '''
         This method write backs the variable data back into the file
@@ -180,49 +80,141 @@ class Setup:
         try:
             namelist = open(self.filename, 'r')
         except:
-            self.logger.error("Cannot open the file:"+self.setfile+" to write ")
+            self.logger.error("Cannot open the file:"+self.filename+" to write ")
             raise
         data = namelist.readlines()
-        
+
         for name in self.settings:
             values = self.settings[name]
             if type(values).__name__ != 'dict':
-                values =  {'-1': values}
+                values = {'-1': values}
             for index, value in values.iteritems():
                 count = -1
-                var_found = False
                 for line in data:
 #                    print line
                     count = count + 1
                     #find the variable
-                    line_without_comments = self.strip_comments(line)
+                    line_without_comments = strip_comments(line)
                     if line_without_comments == '':
                         continue
                     #print line_without_comments
                     data_name, data_index, data_value = self._get_var_name_value(line_without_comments)
-                    
+
                     if data_name == name:
                         #found the variable line
                         if data_index == index:
-                            if type(data_value).__name__ == 'bool' and type(value).__name__ != 'bool':
-                                  data[count]=self._replace_var_value(line, name, index, data_value, self.bool_settings[name])
-#                                  print 'replaced the boolean value'
-                                  continue
-#                            print 'not the boolean and continueing'
-                            var_found = True
+                            if type(data_value).__name__ == 'bool' \
+                                and \
+                               type(value).__name__ != 'bool':
+                                data[count] = _replace_var_value(line, data_value,\
+                                                                 self.bool_settings[name])
+                                continue
+
                             if data_value == value:
                                 break
                             else:
-                                data[count]=self._replace_var_value(line, name, index, data_value, value)
+                                data[count] = _replace_var_value(line, data_value, value)
+                                break
         namelist.close()
-        namelist = open(self.filename,'w')
+        namelist = open(self.filename, 'w')
         namelist.truncate()
         namelist.writelines(data)
-        namelist.close()                      
-                #variable not found need to append to the file
-#                if not var_found:
-#                    data.append()
-                                
-                    #write back the new value
-                
+        namelist.close()
 
+def _trim(data):
+    """ Trims the sets of lines removing empty lines/whitespaces and removing comments
+    which start with ! """
+    newdata = []
+    while data:
+        line = data.pop(0)
+        line = line.rsplit('!')[0].strip()
+        if line is not '':
+            newdata.append(line)
+    return newdata
+
+def _get_val(vars_dictionary, bool_vars_dictionary, line):
+    """ traverses input string and appends the setting name and its value to dictionary
+    of settings and also if the setting name holds a boolean value then to the dictionary
+    of boolean variables. checks the type and raises error for ambiguous values"""
+
+    logger = logging.getLogger(__name__)
+    name_prefix = line[0][0:2].lower()
+    name = line[0][3:].lower().strip() # 3 -> 0 to keep type info
+    value = line[1].strip()
+
+    if name_prefix == 'ln':
+        if value.find('true') is not -1:
+            if vars_dictionary.has_key(name) != True:
+                vars_dictionary[name] = True
+            bool_vars_dictionary[name] = True
+        elif value.find('false') is not -1:
+            if vars_dictionary.has_key(name) != True:
+                vars_dictionary[name] = False
+            bool_vars_dictionary[name] = False
+        else:
+            raise ValueError('Cannot assign %s to %s, must be boolean' %(value, name))
+
+    elif name_prefix == 'rn' or name_prefix == 'nn':
+        if value.find('.') > -1 or value.find('e') > -1:
+            try:
+                vars_dictionary[name] = float(value)
+            except ValueError:
+                logger.error('Cannot assign %s to %s, must be float')
+                raise
+        else:
+            try:
+                vars_dictionary[name] = int(value)
+            except ValueError:
+                logger.error('Cannot assign %s to %s, must be integer')
+                raise
+    elif name_prefix == 'cn' or name_prefix == 'sn':
+        vars_dictionary[name] = value.strip("'")
+    elif name_prefix == 'cl':
+        name = line[0].split('(')
+        index = name[1].split(')')
+        if name[0].strip() not in vars_dictionary.keys():
+            vars_dictionary[name[0].strip()] = {}
+            vars_dictionary[name[0].strip()][index[0].strip()] = value.strip()
+    else:
+        raise ValueError('%s data type is ambiguous' %line)
+
+def _replace_var_value(original_line, value, new_value):
+    """ replaces the variable name value with new_value in the original_line"""
+    if type(value).__name__ == 'bool':
+        value = str(value).lower()
+        new_value = str(new_value).lower()
+    elif type(value).__name__ == 'str': #an empty string need to replace with ''
+        print value, new_value
+        if value == '':
+            value = '\'\''
+            new_value = '\''+new_value+'\''
+    return original_line.replace(value, new_value, 1)
+
+def _get_var_name(line):
+    """ parses the line to find the name and if it is part of the array '()'
+    then returns name of variable and index of the array. if variable is not
+    array then only variable name and -1 in index"""
+    name_value = line.split("=", 2)
+    name_prefix = name_value[0][0:2].lower()
+    name = name_value[0][3:].lower().strip() # 3 -> 0 to keep type info
+    if name_prefix in ['ln', 'rn', 'nn', 'cn', 'sn']:
+        return name, -1
+    elif name_prefix == 'cl':
+        name = name_prefix[0].split('(')
+        index = name[1].split(')')
+        return name, index
+
+    # Returns tidy dictionary of var names and values
+def _assign(data):
+    """ return a dictionary of variables and also special dictionary for boolean variable """
+    vars_dictionary = {}
+    bool_vars_dictionary = {}
+    for line in data:
+        keyvalue = line.split('=', 2)
+        _get_val(vars_dictionary, bool_vars_dictionary, keyvalue)
+    return vars_dictionary, bool_vars_dictionary
+
+def strip_comments(line):
+    """ strips the comments in the line. removes text after ! """
+    line = line.rsplit('!')[0].strip()
+    return line
