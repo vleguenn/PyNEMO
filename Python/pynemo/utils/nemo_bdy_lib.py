cf5b69093d01f481cae497edf923638c88a5c7c7
jdha <jdha@atlassian.com>
2015-06-17 10:55:32 +0000

    addtion of streamfunction functions and function to identify individual bdy sections and endpoints
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/pynemo/trunk@95 9a1c3b68-90a4-42a4-98f4-a1cd0af2bb7b

--- Python/pynemo/utils/nemo_bdy_lib.py
+++ Python/pynemo/utils/nemo_bdy_lib.py
@@ -2,7 +2,8 @@
  Library of some functions used by multiple classes
  Written by John Kazimierz Farey, Sep 2012
 """
-
+import scipy.spatial as sp
+import numpy as np
 
 def sub2ind(shap, subx, suby):
     """subscript to index of a 1d array"""
@@ -40,3 +41,87 @@ def get_output_tidal_filename(setup_var, const_name, grid_type):
     grid type"""
     return setup_var.settings['dst_dir']+setup_var.settings['fn']+"_bdytide_rotT_"+const_name+ \
            "_grid_"+grid_type.upper()+".nc"
+
+def psi_field(U, V):
+    psiu = np.cumsum(U[1:,:], axis=0) - np.cumsum(V[0,:])
+    psiv = ( np.cumsum(U[:,0]) - np.cumsum(V[:,1:], axis=1).T ).T
+    return psiu[:,1:], psiv[1:,:]
+
+def velocity_field(psi):
+    U = np.diff(psi, n=1, axis=0)
+    V = - np.diff(psi, n=1, axis=1)
+    return U, V
+
+def bdy_sections(nbidta,nbjdta,nbrdta,rw):
+    """Extract individual byd sections
+
+    Keyword arguments:
+    """
+    
+    # TODO Need to put a check in here to STOP if we have E-W wrap
+    # as this is not coded yet
+    
+    # Define the outer most halo
+    outer_rim_i = nbidta[nbrdta==rw]
+    outer_rim_j = nbjdta[nbrdta==rw]
+
+    # Set initial constants
+
+    nbdy = len(outer_rim_i)
+    count = 0
+    flag = 0
+    mark = 0
+    source_tree = sp.cKDTree(zip(outer_rim_i, outer_rim_j)) 
+    id_order = np.ones((nbdy,), dtype=np.int)*source_tree.n
+    id_order[count] = 0 # use index 0 as the starting point 
+    count += 1
+    end_pts = {}
+    nsec = 0
+    
+    # Search for individual sections and order
+
+    while count <= nbdy:
+        
+        lcl_pt = zip([outer_rim_i[id_order[count-1]]],
+                     [outer_rim_j[id_order[count-1]]])
+        junk, an_id = source_tree.query(lcl_pt, k=3, distance_upper_bound=1.1)
+        
+        if an_id[0,1] in id_order:
+            if (an_id[0,2] in id_order) or (an_id[0,2] == source_tree.n) : # we are now at an end point and ready to sequence a section
+                if flag == 0:
+                    flag = 1  
+                    end_pts[nsec] = [id_order[count-1], id_order[count-1]] # make a note of the starting point
+                    id_order[mark] = id_order[count-1]
+                    id_order[mark+1:] = source_tree.n # remove previous values
+                    count = mark + 1
+                else:
+                    i = 0
+                    end_pts[nsec][1] = id_order[count-1] # update the end point of the section
+                    nsec += 1
+                    
+                    while i in id_order:
+                        i += 1
+                        
+                    if count < nbdy:
+                        id_order[count] = i
+                    flag = 0
+                    mark = count
+                    count += 1
+
+            else: # lets add the next available point to the sequence
+                id_order[count] = an_id[0,2]
+                count += 1
+
+        else: # lets add the next available point to the sequence
+            id_order[count] = an_id[0,1]
+            count += 1
+        
+    return id_order, end_pts
+    
+def bdy_transport():
+    """Calculate transport across individual bdy sections
+
+    Keyword arguments:
+    """
+    raise NotImplementedError
+    
