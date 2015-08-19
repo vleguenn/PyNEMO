61bd867079677aa7e49b74dc9ea5202de557da93
sri.nagella <sri.nagella@atlassian.com>
2015-08-19 17:27:12 +0000

    [#1267] updated the unittests
    git-svn-id: https://ccpforge.cse.rl.ac.uk/svn/pynemo/trunk@111 9a1c3b68-90a4-42a4-98f4-a1cd0af2bb7b

--- Python/pynemo/tests/reader_ncml_test.py
+++ Python/pynemo/tests/reader_ncml_test.py
@@ -30,7 +30,7 @@ class Test(unittest.TestCase):
     def testTimeCounter(self):
         init_jnius()
         testpath, file_name = os.path.split(__file__)
-        testfile = os.path.join(testpath, "test2.ncml")
+        testfile = os.path.join(testpath, "testremote.ncml")
         sd = Reader(testfile,0).grid
         dataset = Data2(sd.dataset,"time_counter")
         self.assertEquals(len(dataset), 8, "There should 8 datasets")
@@ -38,7 +38,7 @@ class Test(unittest.TestCase):
     def testVariable(self):
         init_jnius()
         testpath, file_name = os.path.split(__file__)
-        testfile = os.path.join(testpath, "test2.ncml")      
+        testfile = os.path.join(testpath, "testremote.ncml")      
         sd = Reader(testfile,0).grid
         dataset = Data2(sd.dataset,"votemper")
         val = dataset[0,0,0,0]
@@ -49,7 +49,7 @@ class Test(unittest.TestCase):
     def testSrcDataVariable(self):
         init_jnius()
         testpath, file_name = os.path.split(__file__)
-        testfile = os.path.join(testpath, "test2.ncml")
+        testfile = os.path.join(testpath, "testremote.ncml")
         sd = Reader(testfile,0).grid    
         dataset = sd["votemper"]
         val = dataset[0,0,0,0]
@@ -60,7 +60,7 @@ class Test(unittest.TestCase):
     def testRepoMatching(self):
         init_jnius()
         testpath, file_name = os.path.split(__file__)
-        testfile = os.path.join(testpath, "test2.ncml") 
+        testfile = os.path.join(testpath, "testremote.ncml") 
         repo = Reader(testfile,0)
         self.assertAlmostEqual(repo['t']['votemper'][0,0,0,0], 18.945175, 6,"First value should be 18.9")
         self.assertAlmostEqual(repo['t']['votemper'][2,10,0,0], 20.314891, 5,"2, 10, 0,0  value should be 18.9")
@@ -68,7 +68,7 @@ class Test(unittest.TestCase):
     def testGridGroupTimeCounter(self):
         init_jnius()
         testpath, file_name = os.path.split(__file__)
-        testfile = os.path.join(testpath, "test2.ncml")
+        testfile = os.path.join(testpath, "testremote.ncml")
         repo = Reader(testfile,0)
         self.assertEquals(len(repo['t'].time_counter), 8, "Time counter should be 8")
         self.assertEquals(repo['t'].time_counter[0], 691416000, "The first time value doesn't match")
