b9843cae7b1f7b555ff2a8530b645853e4282060
J Polton <jelt@noc.ac.uk>
2017-10-18 10:50:52 +0000

    nemo_bdy_extr_tm3.py - add an if statement because it didn't work for empty self.bdy_z

--- Python/pynemo/nemo_bdy_extr_tm3.py
+++ Python/pynemo/nemo_bdy_extr_tm3.py
@@ -624,7 +624,8 @@ class Extract:
                 dst_bdy[nan_ind] = 0
                 self.logger.info(' post dst_bdy %s %s', np.nanmin(dst_bdy), np.nanmax(dst_bdy))
                 # Remove any data on dst grid that is in land
-                dst_bdy[:,np.isnan(self.bdy_z)] = 0
+                if np.sum(self.bdy_z)>0: # jelt: added the if to the statement since it didn't work for empty self.bdy_z
+                        dst_bdy[:,np.isnan(self.bdy_z)] = 0
                 self.logger.info(' 3 dst_bdy %s %s', np.nanmin(dst_bdy), np.nanmax(dst_bdy))
 
                 # If we have depth dimension
