917d9142e1524dc09733cc06357acdb494e415d5
J Polton <jelt@noc.ac.uk>
2018-02-05 15:04:00 +0000

    e3_to_depth.py edited online with Bitbucket. Added a comment.

--- Python/pynemo/utils/e3_to_depth.py
+++ Python/pynemo/utils/e3_to_depth.py
@@ -7,7 +7,9 @@ Useage: [gdept, gdepw] = e3_to_depth(e3t, e3w, nz)
 '''
 
 import numpy as np
-
+## COMMENT(jelt 5Feb18). Since the move from NEMOv3.6 to NEMOv4 we now need to compute the depth variables from e3[tw] metrics
+## Older namelist_dst.ncml files did not have an e3w definition. This is now needed to reconstruct depth at w-points
+## However if e3w isn't defined in the *ncml file it is likely that PyNEMO will fail here.
 def e3_to_depth(pe3t, pe3w, jpk):
   pdepw      = np.zeros_like(pe3w)
   pdepw[0,:] = 0.
